84be579f91ee4feaff5ab49a059c1092de014ecd
Maria Skrabalekova <mariaskrabalekova@gmail.com>
2023-03-08 16:55:02 +0100

    fix validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
-  validates :rating, numericality: { in: 0..5 }
-  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { in: 0..5, only_integer: true }
 end
