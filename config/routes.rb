016922a25f26860797d2d51866665417b3d9abfe
Maria Skrabalekova <mariaskrabalekova@gmail.com>
2023-03-08 16:23:24 +0100

    implement yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/create'
+  get 'restaurants/new'
+  get 'restaurants/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, except: %i[edit update destroy] do
+    resources :reviews, only: %i[new create]
+  end
 end
