ddd16370af69dcaafaf444684eea10a6f1108130
Maria Skrabalekova <mariaskrabalekova@gmail.com>
2023-03-08 14:38:44 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,20 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'chinese' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+nando = { name: 'Nando', address: '56A High St, Paris E1 6PQ', category: 'belgian' }
+pizza_hut = { name: 'Pizza Hut', address: '12 Chopin, New York E1 6PQ', category: 'italian' }
+naree = { name: 'Naree', address: '30 Wide St, LA E1 6PQ', category: 'japanese' }
+
+[dishoom, pizza_east, nando, pizza_hut, naree].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
